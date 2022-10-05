7a0f4f3c4a3946fcebd9622d0b797c37964492b6
andreww1011 <>
2022-10-04 22:50:52 -0700

    remove BigDecimal128Field

--- src/test/java/com/jamw/jmud/examples/CoulombsLawExample.java
+++ src/test/java/com/jamw/jmud/examples/CoulombsLawExample.java
@@ -24,8 +24,6 @@ import com.jamw.jmud.Expressions;
 import com.jamw.jmud.Measure;
 import com.jamw.jmud.Unit;
 import com.jamw.jmud.Units;
-import static com.jamw.jmud.examples.Utils.equalsWithinUlp;
-import com.jamw.jmud.fields.BigDecimal128Field;
 import com.jamw.jmud.fields.DoubleField;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
@@ -55,9 +53,7 @@ public class CoulombsLawExample {
         Expression charge = solveForCharge(force,distance);
         Unit microCoulomb = Units.micro((Units.COULOMB));
         Measure<DoubleField> chargeDouble = charge.using(DoubleField.factory()).as(microCoulomb);
-        Measure<BigDecimal128Field> chargeBigDecimal = charge.using(BigDecimal128Field.factory()).as(microCoulomb);
         String answerInMicroCoulomb = "1.0006922853220581";
-        assertTrue(equalsWithinUlp(chargeDouble.getField().value(),chargeBigDecimal.getField().value().doubleValue()));
         assertTrue(Double.toString(chargeDouble.getField().value()).equals(answerInMicroCoulomb));
     }
 }
